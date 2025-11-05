882b0a6ebae4a426f2c3183bf07deda05147c2ad
Sebastian <joebartolozzzi@gmail.com>
2025-11-05 18:26:46 -0500

    Create driver.h

---
+++ um/driver/driver.h
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <cstdint>
+#include <vector>
+
+
+#define CODE_RW CTL_CODE(FILE_DEVICE_UNKNOWN, 0x2345, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define CODE_BA CTL_CODE(FILE_DEVICE_UNKNOWN, 0x3456, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define CODE_GET_GUARDED_REGION CTL_CODE(FILE_DEVICE_UNKNOWN, 0x4567, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define CODE_GET_DIR_BASE CTL_CODE(FILE_DEVICE_UNKNOWN, 0x5678, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define CODE_SECURITY 0x9af2b37
+
+
+typedef struct _rw {
+	INT32 security;
+	INT32 process_id;
+	ULONGLONG address;
+	ULONGLONG buffer;
+	ULONGLONG size;
+	BOOLEAN write;
+} rw, * prw;
+
+typedef struct _ba {
+	INT32 security;
+	INT32 process_id;
+	ULONGLONG* address;
+} ba, * pba;
+
+typedef struct _ga {
+	INT32 security;
+	ULONGLONG* address;
+} ga, * pga;
+
+typedef struct _MEMORY_OPERATION_DATA {
+	uint32_t        pid;
+	ULONGLONG* cr3;
+} MEMORY_OPERATION_DATA, * PMEMORY_OPERATION_DATA;
+
+struct memory {
+	HANDLE driver_handle;
+	INT32 process_id;
+	uintptr_t base_addy;
+	uintptr_t cr3;
+	bool find_km() {
+		driver_handle = CreateFileW((L"\\\\.\\MmCopyMemory"), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, 0, NULL);
+
+		if (!driver_handle || (driver_handle == INVALID_HANDLE_VALUE))
+			return false;
+
+		return true;
+	}
+
+	void read_physical(PVOID address, PVOID buffer, DWORD size) {
+		_rw arguments = { 0 };
+
+		arguments.security = CODE_SECURITY;
+		arguments.address = (ULONGLONG)address;
+		arguments.buffer = (ULONGLONG)buffer;
+		arguments.size = size;
+		arguments.process_id = process_id;
+		arguments.write = FALSE;
+
+		DeviceIoControl(driver_handle, CODE_RW, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+	}
+	inline bool reads(const std::uintptr_t address, void* buffer, const std::size_t size)
+	{
+		if (buffer == nullptr || size == 0) {
+			return false;
+		}
+		read_physical(reinterpret_cast<PVOID>(address), buffer, static_cast<DWORD>(size));
+	}
+	void write_physical(PVOID address, PVOID buffer, DWORD size) {
+		_rw arguments = { 0 };
+
+		arguments.security = CODE_SECURITY;
+		arguments.address = (ULONGLONG)address;
+		arguments.buffer = (ULONGLONG)buffer;
+		arguments.size = size;
+		arguments.process_id = process_id;
+		arguments.write = TRUE;
+
+		DeviceIoControl(driver_handle, CODE_RW, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+	}
+
+	uintptr_t fetch_cr3() {
+		uintptr_t cr3 = NULL;
+		_MEMORY_OPERATION_DATA arguments = { 0 };
+
+		arguments.pid = process_id;
+		arguments.cr3 = (ULONGLONG*)&cr3;
+
+		DeviceIoControl(driver_handle, CODE_GET_DIR_BASE, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+
+		return cr3;
+	}
+
+	uintptr_t find_game_image() {
+		uintptr_t image_address = { NULL };
+		_ba arguments = { NULL };
+
+		arguments.security = CODE_SECURITY;
+		arguments.process_id = process_id;
+		arguments.address = (ULONGLONG*)&image_address;
+
+		DeviceIoControl(driver_handle, CODE_BA, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+
+		return image_address;
+	}
+
+	uintptr_t get_reg() {
+		uintptr_t guarded_region_address = { NULL };
+		_ga arguments = { NULL };
+
+		arguments.security = CODE_SECURITY;
+		arguments.address = (ULONGLONG*)&guarded_region_address;
+
+		DeviceIoControl(driver_handle, CODE_GET_GUARDED_REGION, &arguments, sizeof(arguments), nullptr, NULL, NULL, NULL);
+
+		return guarded_region_address;
+	}
+
+	INT32 fing_game_pid(LPCTSTR process_name) {
+		PROCESSENTRY32 pt;
+		HANDLE hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		pt.dwSize = sizeof(PROCESSENTRY32);
+		if (Process32First(hsnap, &pt)) {
+			do {
+				if (!lstrcmpi(pt.szExeFile, process_name)) {
+					CloseHandle(hsnap);
+					process_id = pt.th32ProcessID;
+					return pt.th32ProcessID;
+				}
+			} while (Process32Next(hsnap, &pt));
+		}
+		CloseHandle(hsnap);
+
+		return { NULL };
+	}
+
+
+	template <typename T>
+	T read(uint64_t address) {
+		T buffer{ };
+		read_physical((PVOID)address, &buffer, sizeof(T));
+		return buffer;
+	}
+	template<typename T>
+	bool read_array(uintptr_t address, T out[], size_t len)
+	{
+		for (size_t i = 0; i < len; ++i)
+		{
+			out[i] = read<T>(address + i * sizeof(T));
+		}
+		return true; 
+	}
+	template <typename T>
+	T write(uint64_t address, T buffer) {
+
+		write_physical((PVOID)address, &buffer, sizeof(T));
+		return buffer;
+	}
+
+	bool is_valid(const uint64_t adress)
+	{
+		if (adress <= 0x400000 || adress == 0xCCCCCCCCCCCCCCCC || reinterpret_cast<void*>(adress) == nullptr || adress >
+			0x7FFFFFFFFFFFFFFF) {
+			return false;
+		}
+		return true;
+	}
+
+	template<typename T>
+	std::vector<T> read_O(const std::vector<uint64_t>& addresses) {
+		size_t num_addresses = addresses.size();
+		std::vector<T> results(num_addresses);
+		std::vector<uint8_t> buffer(num_addresses * sizeof(T));
+
+
+		for (size_t i = 0; i < num_addresses; ++i) {
+			read_physical((PVOID)addresses[i], buffer.data() + i * sizeof(T), sizeof(T));
+		}
+
+
+		std::memcpy(results.data(), buffer.data(), num_addresses * sizeof(T));
+
+		return results;
+	}
+
+}Ioctl;
